1e50db80bb9e6515c7f4a2dd93c3cf9b56eb5c76
JM <jmjanzen@gmail.com>
2017-01-08 12:29:40 -0500

    Init simple, inline map
    Will have to move this to modules as well

--- pippers/main.py
+++ pippers/main.py
@@ -3,6 +3,14 @@ import tdl
 from modules import Entity
 
 
+# Constants
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+MAP_WIDTH = SCREEN_WIDTH
+MAP_HEIGHT = SCREEN_HEIGHT - 5
+LIMIT_FPS = 20
+
+
 def handle_keys():
 
     user_input = tdl.event.key_wait()
@@ -29,10 +37,33 @@ def handle_keys():
 # main method
 #
 
-# Constants
-SCREEN_WIDTH = 80
-SCREEN_HEIGHT = 50
-LIMIT_FPS = 20
+class Colo:
+    dark_wall = (0, 0, 100)
+    dark_ground = (50, 50, 150)
+
+class Cell:
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight
+
+def init_map():
+    return [[ Cell(False)
+        for y in range(MAP_HEIGHT) ]
+            for x in range(MAP_WIDTH) ]
+
+def draw_map(map):
+    for y in range(MAP_HEIGHT):
+        for x in range(MAP_WIDTH):
+            wall = map[x][y].block_sight
+            if wall:
+                con.draw_char(x, y, None, fg=None, bg=Colo.dark_wall)
+            else:
+                con.draw_char(x, y, None, fg=None, bg=Colo.dark_ground)
+
 
 tdl.set_font("pippers/res/arial10x10.png", greyscale=True, altLayout=True)
 
@@ -50,11 +81,14 @@ npc    = Entity.Npc(10, 10)
 
 entities = [ npc, player ]
 
+map = init_map()
+
 # Bind entities to our console
 for entity in entities:
     entity.bind_console(con)
 
 while not tdl.event.is_window_closed():
+    draw_map(map)
 
     # Draw new entity positions
     for entity in entities:
